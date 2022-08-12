8c13d7d464f77da542f0f61f9cdec2c419f47712
jhou-pro <jhou.pro@gmail.com>
2022-08-12 19:29:35 +0300

    #1 Synced with latest platform changes.

--- code/teltonika-demo/teltonika-demo-web-server/src/main/java/fielden/webapp/WebUiConfig.java
+++ code/teltonika-demo/teltonika-demo-web-server/src/main/java/fielden/webapp/WebUiConfig.java
@@ -7,19 +7,16 @@ import org.apache.commons.lang3.StringUtils;
 import fielden.config.Modules;
 import fielden.config.personnel.PersonWebUiConfig;
 import fielden.personnel.Person;
-
 import ua.com.fielden.platform.basic.config.Workflows;
+import ua.com.fielden.platform.entity.AbstractEntity;
+import ua.com.fielden.platform.reflection.TitlesDescsGetter;
 import ua.com.fielden.platform.utils.Pair;
 import ua.com.fielden.platform.web.app.config.IWebUiBuilder;
 import ua.com.fielden.platform.web.interfaces.ILayout.Device;
-
-import ua.com.fielden.platform.entity.AbstractEntity;
-import ua.com.fielden.platform.reflection.TitlesDescsGetter;
-
 import ua.com.fielden.platform.web.resources.webui.AbstractWebUiConfig;
+import ua.com.fielden.platform.web.resources.webui.SecurityMatrixWebUiConfig;
 import ua.com.fielden.platform.web.resources.webui.UserRoleWebUiConfig;
 import ua.com.fielden.platform.web.resources.webui.UserWebUiConfig;
-import ua.com.fielden.platform.web.resources.webui.SecurityMatrixWebUiConfig;
 
 /**
  * App-specific {@link IWebApp} implementation.
@@ -77,8 +74,8 @@ public class WebUiConfig extends AbstractWebUiConfig {
 
         // Users and Personnel Module
         final PersonWebUiConfig personWebUiConfig = PersonWebUiConfig.register(injector(), builder);
-        final UserWebUiConfig userWebUiConfig = new UserWebUiConfig(injector());
-        final UserRoleWebUiConfig userRoleWebUiConfig = new UserRoleWebUiConfig(injector());
+        final UserWebUiConfig userWebUiConfig = UserWebUiConfig.register(injector(), builder);
+        final UserRoleWebUiConfig userRoleWebUiConfig = UserRoleWebUiConfig.register(injector(), builder);
         final SecurityMatrixWebUiConfig securityConfig = SecurityMatrixWebUiConfig.register(injector(), configApp());
 
         // Add user-rated masters and centres to the configuration 
