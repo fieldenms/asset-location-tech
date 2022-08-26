a856aea6e120b70481737591d93148f37302da88
jhou-pro <jhou.pro@gmail.com>
2022-08-27 00:40:01 +0300

    #1 Made travelledDistance not required for all message tables except main MESSAGES table.

--- code/teltonika-demo/teltonika-demo-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgViolatingMessage.java
+++ code/teltonika-demo/teltonika-demo-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/TgViolatingMessage.java
@@ -1,11 +1,14 @@
 package ua.com.fielden.platform.sample.domain;
 
+import java.math.BigDecimal;
+
 import ua.com.fielden.platform.entity.annotation.CompanionObject;
 import ua.com.fielden.platform.entity.annotation.CompositeKeyMember;
 import ua.com.fielden.platform.entity.annotation.IsProperty;
 import ua.com.fielden.platform.entity.annotation.MapEntityTo;
 import ua.com.fielden.platform.entity.annotation.MapTo;
 import ua.com.fielden.platform.entity.annotation.Observable;
+import ua.com.fielden.platform.entity.annotation.Readonly;
 import ua.com.fielden.platform.entity.annotation.Title;
 import ua.com.fielden.platform.gis.gps.AbstractAvlMessage;
 /** 
@@ -24,6 +27,26 @@ public class TgViolatingMessage extends AbstractAvlMessage {
     @Title(value = "Машина", desc = "Машина, з якої було отримано повідомлення")
     @CompositeKeyMember(1)
     private TgMachine machine;
+
+    @IsProperty(precision = 18, scale = 2)
+    @MapTo(value = "distance_")
+    // TODO
+    @Readonly
+    // @Required -- not required, only for TgMessage it is required
+    @Title(value = "Відстань", desc = "Відстань в метрах, яку було пройдено машиною з моменту отримання попереднього повідомлення.")
+    private BigDecimal travelledDistance;
+
+    @Override
+    @Observable
+    public AbstractAvlMessage setTravelledDistance(final BigDecimal travelledDistance) {
+        this.travelledDistance = travelledDistance;
+        return this;
+    }
+
+    @Override
+    public BigDecimal getTravelledDistance() {
+        return travelledDistance;
+    }
 
     @Observable
     public TgViolatingMessage setMachine(final TgMachine machine) {
