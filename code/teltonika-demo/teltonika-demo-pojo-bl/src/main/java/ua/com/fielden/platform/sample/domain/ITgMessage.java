91ab3779578f570fac183d31641add6ed91616b6
jhou-pro <jhou.pro@gmail.com>
2022-10-15 18:20:04 +0300

    #8 Removed unnecessary special logic for Message saving in a companion. Added ability to save message without re-fetching but preserving push-notifications from there.

--- code/teltonika-demo/teltonika-demo-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/ITgMessage.java
+++ code/teltonika-demo/teltonika-demo-pojo-bl/src/main/java/ua/com/fielden/platform/sample/domain/ITgMessage.java
@@ -1,6 +1,10 @@
 package ua.com.fielden.platform.sample.domain;
 
+import java.util.Optional;
+
 import ua.com.fielden.platform.dao.IEntityDao;
+import ua.com.fielden.platform.entity.query.fluent.fetch;
+import ua.com.fielden.platform.types.either.Either;
 
 /** 
  * Companion object for entity {@link TgMessage}.
@@ -9,5 +13,5 @@ import ua.com.fielden.platform.dao.IEntityDao;
  *
  */
 public interface ITgMessage extends IEntityDao<TgMessage> {
-
+    Either<Long, TgMessage> save(final TgMessage entity, final Optional<fetch<TgMessage>> maybeFetch);
 }
